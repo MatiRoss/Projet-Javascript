3603bc1273c72af7970ef82ea85c0ffa9890d61f
MatiRoss <mati04.ross@gmail.com>
2021-04-01 20:45:30 +0200

    feat : Ajout du jeu shifumi sur la page Games (terminé) #15

--- scripts/games.js
+++ scripts/games.js
@@ -4,104 +4,74 @@ $(document).ready(function () {
     let divResult = document.querySelector('#resultGame')
     let divComputer = document.querySelector('#computerChoice')
 
-    let player = document.querySelector('#playerChoice p')
-    let comp = document.querySelector('#computerChoice p')
-    let playerChoice = player.text
-    let compChoice = comp.text
-
     let buttonRock = document.querySelector('#rock')
     let buttonPaper = document.querySelector('#paper')
     let buttonScissors = document.querySelector('#scissors')
 
+    let playerP = document.createElement('p')
+    let compP = document.createElement('p')
+    let playerChoice
+    let compChoice
+
     let win = document.createElement('p')
     let lose = document.createElement('p')
     let tied = document.createElement('p')
+
     win.textContent = 'Vous avez gagné!'
     lose.textContent = 'Vous avez perdu...'
     tied.textContent = 'Egalité!'
 
-    let rock = document.createElement('p')
-    rock.textContent = buttonRock.value
-    let paper = document.createElement('p')
-    paper.textContent = buttonPaper.value
-    let scissors = document.createElement('p')
-    scissors.textContent = buttonScissors.value
-
-
-    let compChoices = [
+    const choices = [
         "Pierre",
         "Feuille",
         "Ciseaux"
     ]
 
-    buttonRock.addEventListener('click', function (evt) {
-        divPlayer.innerHTML = ""
-        divPlayer.appendChild(rock)
-        getComputerChoice()
-        findWinner()
-        console.log(playerChoice)
-        console.log(compChoice)
-    })
-    buttonPaper.addEventListener('click', function (evt) {
-        divPlayer.innerHTML = ""
-        divPlayer.appendChild(paper)
-        getComputerChoice()
-        findWinner()
-        console.log(player)
-        console.log(compChoice)
-    })
-    buttonScissors.addEventListener('click', function (evt) {
-        divPlayer.innerHTML = ""
-        divPlayer.appendChild(scissors)
-        getComputerChoice()
-        findWinner()
-        console.log(playerChoice)
-        console.log(compChoice)
-    })
 
     function getComputerChoice() {
 
-        let rockComp = document.createElement('p')
-        let paperComp = document.createElement('p')
-        let scissorsComp = document.createElement('p')
-        rockComp.textContent = compChoices[0]
-        paperComp.textContent = compChoices[1]
-        scissorsComp.textContent = compChoices[2]
-
-        let numberChoice = Math.floor(Math.random() * 3);
         divComputer.innerHTML = ""
-        switch (numberChoice) {
+        let randomNumber = Math.floor(Math.random() * 3)
+
+        switch (randomNumber) {
             case 0 :
-                divComputer.appendChild(rockComp)
+                compChoice = choices[0]
+                compP.textContent = compChoice
+                divComputer.appendChild(compP)
                 break;
             case 1 :
-                divComputer.appendChild(paperComp)
+                compChoice = choices[1]
+                compP.textContent = compChoice
+                divComputer.appendChild(compP)
                 break;
             case 2 :
-                divComputer.appendChild(scissorsComp)
+                compChoice = choices[2]
+                compP.textContent = compChoice
+                divComputer.appendChild(compP)
                 break;
         }
     }
 
     function findWinner() {
 
+        divResult.innerHTML = ""
+
         if (playerChoice === compChoice) {
             divResult.appendChild(tied)
-        }
-        if (playerChoice === rock.textContent) {
-            if (compChoice === scissors.textContent) {
+        } else if (playerChoice === choices[0]) {
+            if (compChoice === choices[2]) {
                 divResult.appendChild(win)
             } else {
                 divResult.appendChild(lose)
             }
-        } else if (playerChoice === paper.textContent) {
-            if (compChoice === rock.textContent) {
+        } else if (playerChoice === choices[1]) {
+            if (compChoice === choices[0]) {
                 divResult.appendChild(win)
             } else {
                 divResult.appendChild(lose)
             }
-        } else if (playerChoice === scissors.textContent) {
-            if (compChoice === paper.textContent) {
+        } else if (playerChoice === choices[2]) {
+            if (compChoice === choices[1]) {
                 divResult.appendChild(win)
             } else {
                 divResult.appendChild(lose)
@@ -109,48 +79,32 @@ $(document).ready(function () {
         }
     }
 
-    /*
+    buttonRock.addEventListener('click', function (evt) {
+        divPlayer.innerHTML = ""
+        playerChoice = choices[0]
+        playerP.textContent = playerChoice
+        divPlayer.appendChild(playerP)
+        getComputerChoice()
+        findWinner()
 
-            function findWinner(playerChoice, computerChoice) {
+    })
+    buttonPaper.addEventListener('click', function (evt) {
+        divPlayer.innerHTML = ""
+        playerChoice = choices[1]
+        playerP.textContent = playerChoice
+        divPlayer.appendChild(playerP)
+        getComputerChoice()
+        findWinner()
 
-                } else if (playerChoice === computerChoice) {
-                    return "Tied : égalité";
-                } else if (playerChoice === "rock") {
-                    if (computerChoice === "scissors") {
-                        return "Won : l'utilisateur a gagné";
-                    } else {
-                        return "Lost : l'utilisateur a perdu";
-                    }
-                } else if (playerChoice === "paper") {
-                    if (computerChoice === "rock") {
-                        return "Won : l'utilisateur a gagné";
-                    } else {
-                        return "Lost : l'utilisateur a perdu";
-                    }
-                } else if (playerChoice === "scissors") {
-                    if (computerChoice === "paper") {
-                        return "Won : l'utilisateur a gagné";
-                    } else {
-                        return "Lost : l'utilisateur a perdu";
-                    }
-                } else {
-                    return "Oups, there is an issue, this case wouldn't happen";
-                }
-            }
+    })
+    buttonScissors.addEventListener('click', function (evt) {
+        divPlayer.innerHTML = ""
+        playerChoice = choices[2]
+        playerP.textContent = playerChoice
+        divPlayer.appendChild(playerP)
+        getComputerChoice()
+        findWinner()
 
-
-
-            function playGame() {
-                let uChoice = getPlayerChoice();
-                let computerChoice = getComputerChoice();
-                console.log("player : " + uChoice);
-                console.log("computer : " + computerChoice);
-                console.log(findWinner(uChoice, computerChoice));
-            }
-
-
-
-            playGame();
-        */
+    })
 
 })
